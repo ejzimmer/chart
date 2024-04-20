f1814cd255b004aec6410eb379b77a973b57a430
Erin Zimmer <ejzimmer@gmail.com>
2024-04-20 14:35:29 +1000

    persist pattern

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import React, { useCallback, useMemo, useRef, useState } from "react"
+// pick colours
+// draw lines
+// clear pattern
+// clear stitch
+// redraw pattern when grid resizes
+// make pretty, a11y, error handling
+// publish
 
 const GAUGE_KEY = "gauge"
+const PATTERN_KEY = "pattern"
 const CM_TO_PX = 37.8
 
+type Grid = (string | undefined)[][]
+
+const persistPattern = (pattern: Grid) => {
+  localStorage.setItem(PATTERN_KEY, JSON.stringify(pattern))
+}
+const initialisePattern = (rows: number, columns: number) => {
+  const grid: Grid = Array.from({ length: rows }).map(() =>
+    Array.from({ length: columns })
+  )
+  const persistedPattern = localStorage.getItem(PATTERN_KEY)
+  if (!persistedPattern) return grid
+
+  JSON.parse(persistedPattern).forEach((row: string[], rowNumber: number) => {
+    row.forEach((cell, column) => {
+      if (cell) {
+        grid[rowNumber][column] = cell
+      }
+    })
+  })
+
+  return grid
+}
 const getGaugeFromStorage = () => {
   const value = localStorage.getItem(GAUGE_KEY)
   if (!value) return
@@ -14,7 +44,7 @@ const getGaugeFromStorage = () => {
   }
 }
 
-const COLOURS = ["transparent", "currentColor"]
+const COLOURS = ["currentColor", "transparent"]
 
 function App() {
   const rowsInput = useRef<HTMLInputElement>(null)
@@ -28,13 +58,17 @@ function App() {
     const rowHeight = (10 / (gauge?.rows ?? 10)) * CM_TO_PX
     const stsPerRow = window.screen.width / stitchWidth
     const numberOfRows = window.screen.height / rowHeight
-    const numberOfStitches = stsPerRow * numberOfRows
 
-    return { stitchWidth, rowHeight, stsPerRow, numberOfStitches }
+    return {
+      stitchWidth: `${stitchWidth}px`,
+      rowHeight: `${rowHeight}px`,
+      stsPerRow,
+      numberOfRows,
+    }
   }, [gauge])
 
-  const [stitches, setStitches] = useState<Array<string>>(
-    Array.from({ length: layout.numberOfStitches }).map(() => COLOURS[0])
+  const [stitches, setStitches] = useState<Grid>(() =>
+    initialisePattern(layout.numberOfRows, layout.stsPerRow)
   )
 
   const onSubmit = (event: React.FormEvent) => {
@@ -48,19 +82,27 @@ function App() {
     localStorage.setItem(GAUGE_KEY, JSON.stringify(gauge))
   }
 
-  const changeColour = useCallback((index: number) => {
-    setStitches((stitches) => {
-      const stitch = stitches[index]
+  const drawStitch = useCallback(
+    (row: number, column: number) => {
+      const stitch = stitches[row][column]
       const oldColourIndex = COLOURS.findIndex((colour) => colour === stitch)
       const newColourIndex = (oldColourIndex + 1) % COLOURS.length
 
-      return [
-        ...stitches.slice(0, index),
-        COLOURS[newColourIndex],
-        ...stitches.slice(index + 1),
+      const newStitches = [
+        ...stitches.slice(0, row),
+        [
+          ...stitches[row].slice(0, column),
+          COLOURS[newColourIndex],
+          ...stitches[row].slice(column + 1),
+        ],
+        ...stitches.slice(row + 1),
       ]
-    })
-  }, [])
+
+      setStitches(newStitches)
+      persistPattern(newStitches)
+    },
+    [stitches]
+  )
 
   return (
     <>
@@ -73,38 +115,44 @@ function App() {
         </label>
         <button onClick={onSubmit}>submit</button>
       </form>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: `repeat(${layout.stsPerRow}, ${layout.stitchWidth}px)`,
-          gridAutoRows: `${layout.rowHeight}px`,
-        }}
-      >
-        {stitches.map((stitchColour, index) => (
-          <Stitch
-            key={index}
-            colour={stitchColour}
-            changeColour={() => changeColour(index)}
-          />
+      <div>
+        {stitches.map((row, rowIndex) => (
+          <div key={rowIndex} style={{ display: "flex" }}>
+            {row.map((stitch, columnIndex) => (
+              <Stitch
+                key={`${rowIndex}${columnIndex}`}
+                width={layout.stitchWidth}
+                height={layout.rowHeight}
+                colour={stitch}
+                changeColour={() => drawStitch(rowIndex, columnIndex)}
+              />
+            ))}
+          </div>
         ))}
       </div>
     </>
   )
 }
 
-function Stitch({
-  colour,
-  changeColour,
-}: {
-  colour: string
+type StitchProps = {
+  width: string
+  height: string
+  colour?: string
   changeColour: () => void
-}) {
+}
+
+function Stitch({ width, height, colour, changeColour }: StitchProps) {
   return (
     <div
       style={{
+        width,
+        height,
         borderTop: "2px solid",
         borderRight: "2px solid",
-        backgroundColor: colour,
+        backgroundColor: colour ?? "transparent",
+        cursor: "pointer",
+        flexGrow: 0,
+        flexShrink: 0,
       }}
       onClick={changeColour}
     />
